de0ae5697e1a515e57cec946ea38add655f9d847
tocattabrito <tocattabrito@gmail.com>
2025-10-03 08:00:46 -0300

    Arquivos da aula 9

---
+++ programa2.py
@@ -0,0 +1,36 @@
+
+# -*- coding: utf-8 -*-
+# cabecalho
+print("+----------------------+")
+print("|------Operadores------|")
+print("+----------------------+")
+print()
+# processamento + saidas
+print("Resultados:")
+print("2 + 2 =",2+2)
+print("2 - 2 =",2-2)
+print("2 * 2 =",2*2)
+print("2 / 2 =",2/2)
+print("2 ** 2 =",2**2)
+print("7 % 3 =",7%3)
+
+print("+----------------------+")
+print()
+
+print("+----------------------+")
+print("|------Variaveis-------|")
+print("+----------------------+")
+print()
+
+# entrada
+var1 = 1    # tipo inteiro
+var2 = 1.1  # tipo float
+var3 = "Eu sou uma string"  # tipo string
+var4 = True # tipo boolena
+var4 = False # tipo boolena
+
+# saída
+print("-inteiro =",var1)
+print("-float =",var2)
+print("-string =",var3)
+print("-boolean =",var4)
